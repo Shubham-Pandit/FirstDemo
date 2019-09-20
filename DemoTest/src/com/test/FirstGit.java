1cd00ca7f4bbd0556cb74a1b0b383539950dfd81
Shubham Pandit <55581667+Shubham-Pandit@users.noreply.github.com>
2019-09-20 15:35:28 +0530

    updated
    change

--- DemoTest/src/com/test/FirstGit.java
+++ DemoTest/src/com/test/FirstGit.java
@@ -3,6 +3,6 @@ package com.test;
 public class FirstGit {
 public static void main(String[] args) {
 	System.out.println("Succefully created....");
-	System.out.println("Change 1");
+	System.out.println("1");
 }
 }
